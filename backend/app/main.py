8aa0789519f80f2891e1419451a2d1b2eea225a4
Kingswithkings <kingsuthanaidogu>
2026-03-13 01:03:54 +0000

    Fix Naija House CORS domain

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://global-food-webbot-cdaf.vercel.app",
-        "https://naija-house-webbotapp.vercel.app",
+        "https://naija-house-webbot.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
